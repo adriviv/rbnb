2a8afe5180a8864d70ee3d612b55ebcf5df43c9f
Adrien Viviani <adrien.viv@gmail.com>
2019-04-08 15:31:10 +0800

    sign up rails maybe

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
+###### respond_to :json ??????????????
+
+
   def create
+  @user = User.create(user_params)
+    if @user.save
+      response = { message: 'User created successfully', auth_token: @user.authentication_token }
+      render json: response, status: :created
+     else
+      render json: @user.errors, status: :bad
+     end
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :first_name, :last_name, :address, :credit_card_number)
   end
 end
