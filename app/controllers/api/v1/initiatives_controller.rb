ebd78d7bb9eb35d60b7a2a36440caccbe2c1dfc5
Adrien Viviani <adrien.viv@gmail.com>
2019-04-09 15:20:41 +0800

    rails ok

--- app/controllers/api/v1/initiatives_controller.rb
+++ app/controllers/api/v1/initiatives_controller.rb
@@ -11,8 +11,25 @@ class Api::V1::InitiativesController < Api::V1::BaseController
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    # authorize @project
+    if @project.save
+      render :show, status: :created
+    else
+      render_error
+    end
   end
 
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :location, :category, :rewards, :amount_objective, :video, :image)
+  end
+
+  def render_error
+    render json: {errors: @project.errors.full_messages}
+  end
 
 end
 
